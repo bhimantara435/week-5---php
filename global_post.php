8812fd3c0a3a4add829bc305f1c498b88c78c69f
bhimantara435 <bhimantara.bhima21@gmail.com>
2024-11-21 12:54:28 +0700

    PemrogWEB-Week 5

--- global_post.php
+++ global_post.php
@@ -1,14 +1,15 @@
 <html>
     <body>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
   Name: <input type="text" name="fname">
   <input type="submit">
 </form>
 
 <?php
+$name = $_REQUEST['fname'];
+echo $name;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // collect value of input field
-  $name = $_POST['fname'];
   if (empty($name)) {
     echo "Name is empty";
   } else {
